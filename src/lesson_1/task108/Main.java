da7ce183953b26b926277207ebde116e4a66d148
hypergraphman <r_aitov@mail.ru>
2020-05-19 16:14:01 +0300

    first commit acmp.ru task 1 task 108

---
+++ src/lesson_1/task108/Main.java
@@ -0,0 +1,34 @@
+package lesson_1.task108;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    void solve(Scanner in, PrintWriter out) {
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int s = a + b;
+        out.println('a');
+    }
+
+    void run() {
+        try (
+//                Scanner in = new Scanner(System.in);
+//                PrintWriter out = new PrintWriter(System.out)
+                Scanner in = new Scanner(new FileReader("INPUT.TXT"));
+                PrintWriter out = new PrintWriter(new FileWriter("OUTPUT.TXT"))
+            )
+        {
+            solve(in, out);
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}
